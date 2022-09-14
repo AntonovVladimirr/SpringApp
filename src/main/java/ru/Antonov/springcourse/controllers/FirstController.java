5c8e4a77b3278befda20a57ee5b7ae84bafff7b9
Antonov <vovatrgfhs@list.ru>
2022-09-14 18:20:28 +0300

    change 2

--- src/main/java/ru/Antonov/springcourse/controllers/FirstController.java
+++ src/main/java/ru/Antonov/springcourse/controllers/FirstController.java
@@ -3,6 +3,9 @@ package ru.Antonov.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Antonov Vladimir 04.09.2022
@@ -12,7 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname){
+
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
